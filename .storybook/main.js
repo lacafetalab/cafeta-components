da2889441948d4b93a76e21965460c671438045d
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-23 17:30:33 -0500

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ["../src/**/*.stories.[tj]s"],
+  addons: [
+    "@storybook/addon-notes/register-panel",
+    "@storybook/addon-viewport/register",
+    "@storybook/addon-storysource"
+  ]
+};
