0c38359d162bbeaf7dfbea61f6598f9f21250872
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-27 00:19:48 -0500

    add react components

---
+++ react/src/components.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import { JSX } from 'cafeta-components';
+
+import { defineCustomElements, applyPolyfills } from 'cafeta-components/loader';
+
+applyPolyfills().then(() => defineCustomElements());
+export const MyComponent = /*@__PURE__*/createReactComponent<JSX.MyComponent, HTMLMyComponentElement>('my-component');
