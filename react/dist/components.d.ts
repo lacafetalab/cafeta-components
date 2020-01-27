06167fc85115f5a4a5a72ee6ffdf7d637b0732f2
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-27 00:45:50 -0500

    build react

---
+++ react/dist/components.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+import { JSX } from "cafeta-components";
+export declare const MyComponent: import("react").ForwardRefExoticComponent<JSX.MyComponent & import("react").HTMLAttributes<HTMLMyComponentElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLMyComponentElement>>;
