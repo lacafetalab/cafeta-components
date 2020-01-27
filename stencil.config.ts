21688691bff9467b24e2983aa88ab65cc92f871a
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-26 22:01:38 -0500

    feat: add tailwind

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,16 @@
 import { Config } from "@stencil/core";
 import { sass } from "@stencil/sass";
+import { postcss } from "@stencil/postcss";
+import autoprefixer from "autoprefixer";
+
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./src/**/*.tsx", "./src/index.html"],
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+});
 
 export const config: Config = {
   namespace: "cafeta-components",
+  globalStyle: "src/global/app.scss",
   outputTargets: [
     {
       type: "dist",
@@ -16,5 +24,16 @@ export const config: Config = {
       serviceWorker: null // disable service workers
     }
   ],
-  plugins: [sass()]
+  plugins: [
+    sass(),
+    postcss({
+      plugins: [
+        require("tailwindcss")("./tailwind.config.js"),
+        autoprefixer(),
+        ...(process.env.NODE_ENV === "production"
+          ? [purgecss, require("cssnano")]
+          : [])
+      ]
+    })
+  ]
 };
