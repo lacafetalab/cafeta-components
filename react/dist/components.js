e9577701f486c3ceed4cdaca136376cb4c3ca395
Renzo Telenta <renzo@lacafetalab.pe>
2020-01-27 09:59:06 -0500

    add tailwind config to react project

--- react/dist/components.js
+++ react/dist/components.js
@@ -3,8 +3,6 @@
 /* auto-generated react proxies */
 import { createReactComponent } from "./react-component-lib";
 import { defineCustomElements, applyPolyfills } from "cafeta-components/loader";
-applyPolyfills().then(() => {
-    defineCustomElements(window);
-});
+applyPolyfills().then(() => defineCustomElements(window));
 export const MyComponent = /*@__PURE__*/ createReactComponent("my-component");
 //# sourceMappingURL=components.js.map
