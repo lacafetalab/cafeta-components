cd24030178293a963ebb81280dc29c301dea6914
Renzo Telenta <renzotelenta@gmail.com>
2020-02-04 11:29:15 -0500

    update

--- packages/react/src/components.ts
+++ packages/react/src/components.ts
@@ -1,29 +1,14 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from "./react-component-lib";
+import { createReactComponent } from './react-component-lib';
 
-import { JSX } from "@cafeta/components";
+import { JSX } from 'cafeta-components';
 
-import {
-  defineCustomElements,
-  applyPolyfills
-} from "@cafeta/components/loader";
+import { defineCustomElements, applyPolyfills } from 'cafeta-components/loader';
 
 applyPolyfills().then(() => defineCustomElements());
-export const CcBadge = /*@__PURE__*/ createReactComponent<
-  JSX.CcBadge,
-  HTMLCcBadgeElement
->("cc-badge");
-export const CcButton = /*@__PURE__*/ createReactComponent<
-  JSX.CcButton,
-  HTMLCcButtonElement
->("cc-button");
-export const CcIcon = /*@__PURE__*/ createReactComponent<
-  JSX.CcIcon,
-  HTMLCcIconElement
->("cc-icon");
-export const CcText = /*@__PURE__*/ createReactComponent<
-  JSX.CcText,
-  HTMLCcTextElement
->("cc-text");
+export const CcBadge = /*@__PURE__*/createReactComponent<JSX.CcBadge, HTMLCcBadgeElement>('cc-badge');
+export const CcButton = /*@__PURE__*/createReactComponent<JSX.CcButton, HTMLCcButtonElement>('cc-button');
+export const CcIcon = /*@__PURE__*/createReactComponent<JSX.CcIcon, HTMLCcIconElement>('cc-icon');
+export const CcText = /*@__PURE__*/createReactComponent<JSX.CcText, HTMLCcTextElement>('cc-text');
