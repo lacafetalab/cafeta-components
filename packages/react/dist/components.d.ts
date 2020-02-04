f8cfe43bc2e7bd5562985f8685ce77e5c0763965
Renzo Telenta <renzo@lacafetalab.pe>
2020-02-04 11:10:23 -0500

    stable

--- packages/react/dist/components.d.ts
+++ packages/react/dist/components.d.ts
@@ -1,5 +1,5 @@
 /// <reference types="react" />
-import { JSX } from "cafeta-components";
+import { JSX } from 'cafeta-components';
 export declare const CcBadge: import("react").ForwardRefExoticComponent<JSX.CcBadge & import("react").HTMLAttributes<HTMLCcBadgeElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLCcBadgeElement>>;
 export declare const CcButton: import("react").ForwardRefExoticComponent<JSX.CcButton & import("react").HTMLAttributes<HTMLCcButtonElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLCcButtonElement>>;
 export declare const CcIcon: import("react").ForwardRefExoticComponent<JSX.CcIcon & import("react").HTMLAttributes<HTMLCcIconElement> & import("./react-component-lib/utils").ReactProps & import("react").RefAttributes<HTMLCcIconElement>>;
--- packages/react/dist/components.js
+++ packages/react/dist/components.js
@@ -1,11 +1,11 @@
 /* eslint-disable */
 /* tslint:disable */
 /* auto-generated react proxies */
-import { createReactComponent } from "./react-component-lib";
-import { defineCustomElements, applyPolyfills } from "cafeta-components/loader";
-applyPolyfills().then(() => defineCustomElements(window));
-export const CcBadge = /*@__PURE__*/ createReactComponent("cc-badge");
-export const CcButton = /*@__PURE__*/ createReactComponent("cc-button");
-export const CcIcon = /*@__PURE__*/ createReactComponent("cc-icon");
-export const CcText = /*@__PURE__*/ createReactComponent("cc-text");
+import { createReactComponent } from './react-component-lib';
+import { defineCustomElements, applyPolyfills } from 'cafeta-components/loader';
+applyPolyfills().then(() => defineCustomElements());
+export const CcBadge = /*@__PURE__*/ createReactComponent('cc-badge');
+export const CcButton = /*@__PURE__*/ createReactComponent('cc-button');
+export const CcIcon = /*@__PURE__*/ createReactComponent('cc-icon');
+export const CcText = /*@__PURE__*/ createReactComponent('cc-text');
 //# sourceMappingURL=components.js.map
