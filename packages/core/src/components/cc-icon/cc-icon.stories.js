84fc7e6f7133d2c337884cf7b2737048a84e7330
Renzo Telenta <renzotelenta@gmail.com>
2020-02-07 13:48:25 -0500

    update icons

--- packages/core/src/components/cc-icon/cc-icon.stories.js
+++ packages/core/src/components/cc-icon/cc-icon.stories.js
@@ -7,15 +7,25 @@ export default { title: "Icon", decorators: [withKnobs] };
 export const Icon = () => {
   const label = "Names";
   const options = {
-    file: "file",
-    edit: "edit"
+    user: "user"
   };
-  const defaultValue = "file";
+  const defaultValue = "user";
   const groupId = "GROUP-ID1";
 
   const value = select(label, options, defaultValue, groupId);
 
-  return `<cc-icon name="${value}"></cc-icon>`;
+  const label2 = "Colors";
+  const options2 = {
+    "color-text-01": "color-text-01",
+    "color-text-02": "color-text-02",
+    "color-text-03": "color-text-03"
+  };
+  const defaultValue2 = "color-text-01";
+  const groupId2 = "GROUP-ID1";
+
+  const value2 = select(label2, options2, defaultValue2, groupId2);
+
+  return `<cc-icon name="${value}" color="${value2}"></cc-icon>`;
 };
 
 Icon.story = {
--- packages/core/src/components/cc-icon/cc-icon.tsx
+++ packages/core/src/components/cc-icon/cc-icon.tsx
@@ -1,24 +1,48 @@
-import { Component, Prop, h, getAssetPath, Host } from "@stencil/core";
+import { Component, Prop, h, Host } from "@stencil/core";
+import iconPaths from "./selection";
 
 @Component({
   tag: "cc-icon",
   styleUrl: "cc-icon.scss",
-  assetsDirs: ["assets"],
   shadow: true
 })
 export class CcIcon {
   @Prop() name: string;
   @Prop() size: number = 24;
+  @Prop() color: "color-text-01" | "color-text-02" | "color-text-03" =
+    "color-text-01";
+
+  private getPath = (iconName: string) => {
+    const icon = iconPaths.icons.find(
+      icon => icon.properties.name === iconName
+    );
+
+    if (icon) {
+      return icon.icon.paths.join(" ");
+    } else {
+      return "";
+    }
+  };
 
   render() {
+    if (this.name === "") {
+      return null;
+    }
+
     return (
-      <Host>
-        <svg class="cc-icon" width={this.size} height={this.size}>
-          <use
-            xlinkHref={`${getAssetPath(`./assets/feather-sprite.svg`)}#${
-              this.name
-            }`}
-          />
+      <Host
+        class={{
+          "cc-icon--color-text-02": this.color === "color-text-02",
+          "cc-icon--color-text-03": this.color === "color-text-03"
+        }}
+      >
+        <svg
+          class="cc-icon"
+          width={this.size}
+          height={this.size}
+          viewBox={"0 0 1024 1024"}
+        >
+          <path d={this.getPath(this.name)} />
         </svg>
       </Host>
     );
