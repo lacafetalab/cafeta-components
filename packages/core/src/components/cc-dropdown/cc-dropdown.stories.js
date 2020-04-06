206cb802737bc42acaa90f6d1d3b6e0ef8702335
Renzo Telenta <renzotelenta@gmail.com>
2020-04-06 16:51:11 -0500

    wip

--- packages/core/src/components/cc-dropdown/cc-dropdown.stories.js
+++ packages/core/src/components/cc-dropdown/cc-dropdown.stories.js
@@ -5,10 +5,10 @@ import { object } from "@storybook/addon-knobs";
 export default { title: "Dropdown", decorators: [withKnobs] };
 
 export const Dropdown = () => {
-  const itemsWrapper ={
+  const itemsWrapper = {
     choices: [
-      {value: 'aaa', label: 'bbbb'}, 
-      {value: 'cccccc', label: 'ddddddd'}
+      { value: "aaa", label: "bbbb" },
+      { value: "cccccc", label: "ddddddd" }
     ]
   };
 
@@ -17,9 +17,7 @@ export const Dropdown = () => {
   const groupId3 = "GROUP-ID1";
   const value3 = boolean(label3, defaultValue3, groupId3);
 
-  const props = [
-    value3 ? "expand" : ""
-  ].join(" ");
+  const props = [value3 ? "expand" : ""].join(" ");
 
   return `
     <div class="p-lg">
@@ -43,17 +41,45 @@ export const Dropdown = () => {
         </div>
       </section>
     </div>
-    <script>
-      (() => {
-        document.querySelectorAll('cc-dropdown').forEach((item) => {
-          console.log('asdsadjsksajdhask=====>', item, ${itemsWrapper.choices});
-          item.choices = ${itemsWrapper.choices};
-        })
-      })()
-    </script>
-  `
+  `;
 };
 
+export const Dropdown2 = () => {
+  const itemsWrapper = {
+    choices: [
+      { value: "aaa", label: "bbbb" },
+      { value: "cccccc", label: "ddddddd" }
+    ]
+  };
+
+  const dropdown = document.createElement("cc-dropdown");
+  dropdown.choices = itemsWrapper.choices;
+
+  const wrap = document.createElement("div");
+  wrap.innerHTML = `
+  <div class="p-lg">
+    <h1 class="heading-01">Dropdown primary</h1>
+    <section class="py-xlg color-primary font-black">
+      <h2 class="text-subheading-02 mb-lg font-regular">Dropdown Inline</h1>
+      <div>
+        <div class="mr-lg mt-lg flex flex-col align-middle" id="drop1">
+          <h2 class="text-center font-regular">size: <strong class="font-bold">default</strong></h1>
+        </div>
+      </div>
+    </section>
+    <section class="py-xlg color-primary font-black">
+      <h2 class="text-subheading-02 mb-lg font-regular">Dropdown with Label</h1>
+      <div >
+        <div class="mr-lg mt-lg flex flex-col align-middle">
+          <h2 class="text-center font-regular">size: <strong class="font-bold">default</strong></h1>
+        </div>
+      </div>
+    </section>
+  </div>`;
+
+  wrap.querySelector("#drop1").appendChild(dropdown);
+  return wrap;
+};
 
 Dropdown.story = {
   parameters: {
