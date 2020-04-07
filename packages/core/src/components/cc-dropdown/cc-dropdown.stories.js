2e63de64da057daade443c597cf7851f12520172
Carlos Huamani <carlos.hs.92@gmail.com>
2020-04-06 20:27:03 -0500

    feat(dropdown): first version

--- packages/core/src/components/cc-dropdown/cc-dropdown.stories.js
+++ packages/core/src/components/cc-dropdown/cc-dropdown.stories.js
@@ -1,6 +1,5 @@
 import notes from "./readme.md";
 import { withKnobs, boolean } from "@storybook/addon-knobs/html";
-import { object } from "@storybook/addon-knobs";
 
 export default { title: "Dropdown", decorators: [withKnobs] };
 
@@ -12,59 +11,25 @@ export const Dropdown = () => {
     ]
   };
 
-  const label3 = "Expand";
+  const dropdown = document.createElement("cc-dropdown");
+
+  const label3 = "Error";
   const defaultValue3 = false;
   const groupId3 = "GROUP-ID1";
   const value3 = boolean(label3, defaultValue3, groupId3);
 
-  const props = [value3 ? "expand" : ""].join(" ");
+  const label2 = "Disabled";
+  const defaultValue2 = false;
+  const groupId2 = "GROUP-ID1";
+  const value2 = boolean(label2, defaultValue2, groupId2);
 
-  return `
-    <div class="p-lg">
-      <h1 class="heading-01">Dropdown primary</h1>
-      <section class="py-xlg color-primary font-black">
-        <h2 class="text-subheading-02 mb-lg font-regular">Dropdown Inline</h1>
-        <div ${value3 ? "" : 'class="flex align-middle"'}>
-          <div class="mr-lg mt-lg flex flex-col align-middle">
-            <h2 class="text-center font-regular">size: <strong class="font-bold">default</strong></h1>
-            <cc-dropdown ${props} />
-          </div>
-        </div>
-      </section>
-      <section class="py-xlg color-primary font-black">
-        <h2 class="text-subheading-02 mb-lg font-regular">Dropdown with Label</h1>
-        <div ${value3 ? "" : 'class="flex align-middle"'}>
-          <div class="mr-lg mt-lg flex flex-col align-middle">
-            <h2 class="text-center font-regular">size: <strong class="font-bold">default</strong></h1>
-            <cc-dropdown ${props} label="Label" />
-          </div>
-        </div>
-      </section>
-      <script type="text/javascript">
-        (() => {
-          alert('hola mundo')
-          document.querySelectorAll('cc-dropdown').forEach((item) => {
-            console.log('asdsadjsksajdhask=====>', item, ${
-              itemsWrapper.choices
-            });
-            item.choices = ${itemsWrapper.choices};
-          })
-        })()
-      </script>
-    </div>
-  `;
-};
-
-export const Dropdown2 = () => {
-  const itemsWrapper = {
-    choices: [
-      { value: "aaa", label: "bbbb" },
-      { value: "cccccc", label: "ddddddd" }
-    ]
-  };
-
-  const dropdown = document.createElement("cc-dropdown");
   dropdown.choices = itemsWrapper.choices;
+  if (value3) {
+    dropdown.error = value3;
+  }
+  if (value2) {
+    dropdown.disabled = value2;
+  }
 
   const wrap = document.createElement("div");
   wrap.innerHTML = `
