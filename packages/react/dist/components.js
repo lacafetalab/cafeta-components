bcae7cbbc7d2b616d9dc260ea3e2435a5f90d346
Renzo Telenta <renzo@lacafetalab.pe>
2020-04-16 04:28:12 -0500

    chore: build input component and button without shadow dom

--- packages/react/dist/components.js
+++ packages/react/dist/components.js
@@ -7,6 +7,7 @@ applyPolyfills().then(() => defineCustomElements());
 export const CcButton = /*@__PURE__*/ createReactComponent('cc-button');
 export const CcDropdown = /*@__PURE__*/ createReactComponent('cc-dropdown');
 export const CcIcon = /*@__PURE__*/ createReactComponent('cc-icon');
+export const CcInput = /*@__PURE__*/ createReactComponent('cc-input');
 export const CcText = /*@__PURE__*/ createReactComponent('cc-text');
 export const CcTextarea = /*@__PURE__*/ createReactComponent('cc-textarea');
 //# sourceMappingURL=components.js.map
