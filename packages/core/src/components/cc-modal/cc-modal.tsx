e091bab305f22be55e4d5839328123cac86ed56c
Renzo Telenta <renzo@lacafetalab.pe>
2020-04-21 11:29:23 -0500

    feat(modal): add x button to modal

--- packages/core/src/components/cc-modal/cc-modal.tsx
+++ packages/core/src/components/cc-modal/cc-modal.tsx
@@ -11,17 +11,20 @@ import {
 } from "@stencil/core";
 
 @Component({
-  tag: "cc-modal-base",
-  styleUrl: "cc-modal-base.scss",
+  tag: "cc-modal",
+  styleUrl: "cc-modal.scss",
   scoped: true
 })
-export class CcModalBase {
+export class CcModal {
   private overlayEl: HTMLElement;
   private wrapEl: HTMLElement;
   private contentEl: HTMLElement;
 
   @Prop() size: "sm" | "md" = "md";
   @Prop() visible: boolean = false;
+  @Prop() color: "primary" | "secondary" = "primary";
+  @Prop() hideCloseButton: boolean = false;
+
   @Element() el: HTMLElement;
 
   @Event() close: EventEmitter;
@@ -41,9 +44,9 @@ export class CcModalBase {
     if (ev.key === "Escape") this.cancelHandler();
   }
 
-  cancelHandler() {
+  cancelHandler = () => {
     this.cancel.emit();
-  }
+  };
 
   closeHandler() {
     this.enableBodyScroll();
@@ -56,18 +59,18 @@ export class CcModalBase {
 
   animatedShow() {
     this.disableBodyScroll();
-    this.overlayEl.classList.remove("modalBase__overlay--hidden");
-    this.overlayEl.classList.add("modalBase__overlay--fadeIn");
+    this.overlayEl.classList.remove("modal__overlay--hidden");
+    this.overlayEl.classList.add("modal__overlay--fadeIn");
 
     this.wrapEl.classList.remove("hidden");
-    this.contentEl.classList.add("modalBase__content--zoomIn");
+    this.contentEl.classList.add("modal__content--zoomIn");
   }
 
   animatedHide() {
-    this.overlayEl.classList.remove("modalBase__overlay--visible");
-    this.overlayEl.classList.add("modalBase__overlay--fadeOut");
+    this.overlayEl.classList.remove("modal__overlay--visible");
+    this.overlayEl.classList.add("modal__overlay--fadeOut");
 
-    this.contentEl.classList.add("modalBase__content--zoomOut");
+    this.contentEl.classList.add("modal__content--zoomOut");
   }
 
   enableBodyScroll() {
@@ -79,31 +82,31 @@ export class CcModalBase {
   }
 
   otherModalIsVisible() {
-    return !![...(document.querySelectorAll("cc-modal-base") as any)].find(
+    return !![...(document.querySelectorAll("cc-modal") as any)].find(
       e => e.visible
     ).length;
   }
 
   overlayAnimationEnd = (e: AnimationEvent) => {
     if (e.animationName === "fadeInModal") {
-      this.overlayEl.classList.remove("modalBase__overlay--fadeIn");
-      this.overlayEl.classList.add("modalBase__overlay--visible");
+      this.overlayEl.classList.remove("modal__overlay--fadeIn");
+      this.overlayEl.classList.add("modal__overlay--visible");
     }
 
     if (e.animationName === "fadeOutModal") {
-      this.overlayEl.classList.add("modalBase__overlay--hidden");
-      this.overlayEl.classList.remove("modalBase__overlay--fadeOut");
+      this.overlayEl.classList.add("modal__overlay--hidden");
+      this.overlayEl.classList.remove("modal__overlay--fadeOut");
       this.closeHandler();
     }
   };
 
   contentAnimationEnd = (e: AnimationEvent) => {
     if (e.animationName === "zoomInModal") {
-      this.contentEl.classList.remove("modalBase__content--zoomIn");
+      this.contentEl.classList.remove("modal__content--zoomIn");
     }
 
     if (e.animationName === "zoomOutModal") {
-      this.contentEl.classList.remove("modalBase__content--zoomOut");
+      this.contentEl.classList.remove("modal__content--zoomOut");
       this.wrapEl.classList.add("hidden");
     }
   };
@@ -120,30 +123,43 @@ export class CcModalBase {
 
   render() {
     return (
-      <Host class="modalBase">
+      <Host class="modal">
         <cc-modal-controller modalRef={this.el}></cc-modal-controller>
 
         <div
           ref={el => (this.overlayEl = el)}
-          class="modalBase__overlay modalBase__overlay--hidden"
+          class="modal__overlay modal__overlay--hidden"
           onAnimationEnd={this.overlayAnimationEnd}
         />
 
         <div
           ref={el => (this.wrapEl = el)}
-          class="modalBase__wrap hidden"
+          class="modal__wrap hidden"
           onClick={this.handleWrapClick}
         >
           <div
             ref={el => (this.contentEl = el)}
             onAnimationEnd={this.contentAnimationEnd}
             class={{
-              modalBase__content: true,
-              "modalBase__content--sm": this.size === "sm",
-              "modalBase__content--md": this.size === "md"
+              modal__content: true,
+              "modal__content--sm": this.size === "sm",
+              "modal__content--md": this.size === "md"
             }}
           >
             <slot />
+
+            {!this.hideCloseButton && (
+              <button
+                class={{
+                  modal__close: true,
+                  "modal__close--primary": this.color === "primary",
+                  "modal__close--secondary": this.color === "secondary"
+                }}
+                onClick={this.cancelHandler}
+              >
+                <cc-icon size={16} name="x" />
+              </button>
+            )}
           </div>
         </div>
       </Host>
