7e7e9aec0b16f8040f4da42c72821f3685379824
Renzo Telenta <renzo@lacafetalab.pe>
2020-05-12 03:12:49 -0500

    feat(tabs): add tabs tags

---
+++ packages/core/src/components/cc-tabs-tags/cc-tabs-tags.tsx
@@ -0,0 +1,78 @@
+import {
+  Component,
+  Prop,
+  State,
+  h,
+  Host,
+  EventEmitter,
+  Event
+} from "@stencil/core";
+import { TabOption } from "../../utils/types/TabOption";
+
+@Component({
+  tag: "cc-tabs-tags",
+  styleUrl: "cc-tabs-tags.scss",
+  shadow: true
+})
+export class CcTabsTags {
+  @Prop() color?: "primary" | "secondary" = "primary";
+  @Prop() options?: TabOption[] = [];
+
+  @State() _options: TabOption[];
+
+  @Event() changeOption: EventEmitter<TabOption>;
+
+  updateOptions(index: number) {
+    const newOptions = [...this._options].map(option => ({
+      ...option,
+      active: false
+    }));
+
+    newOptions[index].active = true;
+    this._options = newOptions;
+  }
+
+  handleOptionClick = (index: number) => () => {
+    const option = this._options[index];
+
+    if (option.disabled) {
+      return;
+    }
+
+    this.updateOptions(index);
+    this.changeOption.emit(option.value);
+  };
+
+  componentWillLoad() {
+    this._options = this.options;
+  }
+
+  render() {
+    return (
+      <Host>
+        <div
+          class={{
+            tabTags: true
+          }}
+        >
+          <div class="tabTags__wrap">
+            {this._options.map((option, index) => (
+              <button
+                key={`tabTagItem_${index}`}
+                class={{
+                  tabTags__button: true,
+                  "tabTags__button--secondary": this.color === "secondary",
+                  "tabTags__button--active": option.active,
+                  "tabTags__button--disabled": option.disabled
+                }}
+                onClick={this.handleOptionClick(index)}
+              >
+                {option.text}
+              </button>
+            ))}
+          </div>
+        </div>
+      </Host>
+    );
+  }
+}
---
+++ packages/core/src/components/cc-tabs-tags/readme.md
@@ -0,0 +1,25 @@
+# cc-tabs-tags
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property  | Attribute | Description | Type                       | Default     |
+| --------- | --------- | ----------- | -------------------------- | ----------- |
+| `color`   | `color`   |             | `"primary" \| "secondary"` | `"primary"` |
+| `options` | --        |             | `TabOption[]`              | `[]`        |
+
+
+## Events
+
+| Event          | Description | Type                                                                               |
+| -------------- | ----------- | ---------------------------------------------------------------------------------- |
+| `changeOption` |             | `CustomEvent<{ active?: boolean; disabled?: boolean; text: string; value: any; }>` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
