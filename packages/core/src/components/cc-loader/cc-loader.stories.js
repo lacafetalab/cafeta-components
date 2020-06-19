7e90a10b2ce07f8e8378c23737464782868f6ced
Harol <darksxd@live.com>
2020-06-19 13:45:15 -0500

    adding size on loader knobs

--- packages/core/src/components/cc-loader/cc-loader.stories.js
+++ packages/core/src/components/cc-loader/cc-loader.stories.js
@@ -4,6 +4,7 @@ import { withKnobs, number } from "@storybook/addon-knobs/html";
 export default { title: "Loader", decorators: [withKnobs] };
 
 export const Loader = () => {
+  const size = number("Size", 50);
   return `
     <div class="p-xlg">
       <cc-loader size="${size}">< /cc-loader>
