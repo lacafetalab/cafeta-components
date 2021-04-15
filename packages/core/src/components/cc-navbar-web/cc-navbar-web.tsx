2a247163e5b819732d57f28f8588dc0033f0b146
Carlos Huamani <carlos.hs.92@gmail.com>
2021-04-15 10:31:30 -0500

    fix(navbarweb): update packages and text loading

--- packages/core/src/components/cc-navbar-web/cc-navbar-web.tsx
+++ packages/core/src/components/cc-navbar-web/cc-navbar-web.tsx
@@ -16,7 +16,6 @@ import "choicesjs-stencil";
   styleUrls: ["cc-navbar-web.scss"],
   shadow: false
 })
-
 export class CcNavbarWeb {
   @Prop() iconOnly: boolean = false;
   @Prop() disabled: boolean = false;
@@ -27,71 +26,65 @@ export class CcNavbarWeb {
   @Event() changeChoice: EventEmitter;
   @Event() clickProfileMenu: EventEmitter;
 
-
-  componentWillLoad() {
-  }
+  componentWillLoad() {}
 
   componentDidLoad() {
     this.loadMenu();
   }
 
   private loadMenu(): void {
-    this.datalist =  [
-      { url: "123", label: "Option 1", icon: "home", active: true},
-      { url: "124", label: "Opción 2", icon: "calendar", active: false},
-      { url: "125", label: "Opción 3", icon: "user", active: false},
-      { url: "126", label: "Opción 4", icon: "globe", active: false},
-      { url: "127", label: "Opción 5", icon: "book", active: false},
-      { url: "128", label: "Opción 6", icon: "dollar-sign", active: false},
-      { url: "123", label: "Option 7", icon: "send", active: false},
-    ]
+    this.datalist = [
+      { url: "123", label: "Option 1", icon: "home", active: true },
+      { url: "124", label: "Opción 2", icon: "calendar", active: false },
+      { url: "125", label: "Opción 3", icon: "user", active: false },
+      { url: "126", label: "Opción 4", icon: "globe", active: false },
+      { url: "127", label: "Opción 5", icon: "book", active: false },
+      { url: "128", label: "Opción 6", icon: "dollar-sign", active: false },
+      { url: "123", label: "Option 7", icon: "send", active: false }
+    ];
   }
-
 
   toggleProfileMenu = e => {
     e.stopPropagation();
   };
 
-
   render() {
-
-
-
     return (
       <Host>
-{this.datalist && this.datalist.length
-    ?
-        <div
-          class='navbar'>
-          <div>
-          <cc-button class='navbar__toggle' href="#" target="_blank" ><cc-icon name="menu"
-                   class='menu'
-                  ></cc-icon></cc-button>
-          <ul
-            class={{ 'navbar__list--vertical':true, navbar__list: true}}
-          >
-          {this.datalist.map(list =>
-                <li class={{ navbar__item: true,'navbar__item--selected': list.active}}>
-                  <cc-button fill='clear' href={list.url} target="_blank" >
-                  <cc-icon size={24}                   
-                    name={list.icon}
-                  ></cc-icon>
-                  <span class='navbar__item-text'>{list.label}</span>
-                  </cc-button>
-                </li>,
-          )}
-          </ul>
+        {this.datalist && this.datalist.length ? (
+          <div class="navbar">
+            <div>
+              <cc-button class="navbar__toggle" href="#" target="_blank">
+                <cc-icon name="menu" class="menu"></cc-icon>
+              </cc-button>
+              <ul
+                class={{ "navbar__list--vertical": true, navbar__list: true }}
+              >
+                {this.datalist.map(list => (
+                  <li
+                    class={{
+                      navbar__item: true,
+                      "navbar__item--selected": list.active
+                    }}
+                  >
+                    <cc-button fill="clear" href={list.url} target="_blank">
+                      <cc-icon size={24} name={list.icon}></cc-icon>
+                      <span class="navbar__item-text">{list.label}</span>
+                    </cc-button>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-         : <div>Loading...</div>
-        }
+        ) : (
+          <div>Cargando...</div>
+        )}
       </Host>
     );
   }
 }
 
-
-export interface MenuList{ 
+export interface MenuList {
   url: string;
   label: string;
   icon: string;
